314cc9a39e40b50c0471220fc124f12e93da4755
mohamed <amranimo@outlook.fr>
2016-10-16 01:31:22 +0100

    mohamed mis ajour 1:31

--- app/src/main/java/com/example/moh/reminderapp/jourderepos.java
+++ app/src/main/java/com/example/moh/reminderapp/jourderepos.java
@@ -1,7 +1,9 @@
 package com.example.moh.reminderapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class jourderepos extends AppCompatActivity {
 
@@ -9,5 +11,9 @@ public class jourderepos extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_jourderepos);
+    }
+    public void enregistrer(View v){
+        Intent i = new Intent(jourderepos.this, Evenement.class);
+        startActivity(i);
     }
 }
